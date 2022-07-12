7123de304b041bc0e66aee44d8c29fb36bdaea1a
lff <17600370573@163.com>
2022-07-12 21:23:28 +0800

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import IndexRouter from "./router/index"
 import './App.css'
 const App:React.FC = ()=> {
-  return <IndexRouter></IndexRouter>
+  // return <IndexRouter></IndexRouter>
+  return <div>12345</div>
 }
 export default App
